55e5da4e768143cdb4b6f14a5481466ec3fe94cd
Jorge Galvan <jorgegalvan@Jorges-MacBook-Air.local>
2019-11-21 22:06:12 -0600

    new ticket form add to flight, one to many relationship

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,6 +1,18 @@
-var Ticket = require('../models/Ticket');
-var Flight = require('../models/flight');
+var Ticket = require('../models/ticket');
 
 module.exports = {
-
+    new: newTicket,
+    create,
 };
+
+function create(req, res) {
+    flightId = req.params.id;
+    req.body.flight = flightId;
+    Ticket.create(req.body, function(err, ticket) {
+        res.redirect(`/flights/${flightId}`);
+    })
+};
+
+function newTicket(req, res){
+    res.render('tickets/new', {flightId: req.params.id})
+}
--- models/ticket.js
+++ models/ticket.js
@@ -3,16 +3,17 @@ var Schema = mongoose.Schema;
 
 var ticketSchema = new Schema ({
     seat: {
-        type: String
+        type: String,
+        match: /[A-F][1-9]\d?/
     },
     price: {
         type: Number,
         min: 0
     },
     flight: {
-        type: ObjectId,
+        type: Schema.Types.ObjectId,
         ref: 'Flight'
     }
-})
+});
 
 module.exports = mongoose.model('Ticket', ticketSchema);
